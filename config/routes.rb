60dcf6855df2bac37648cbb8e9e258e8be996721
Ribot Fleury <teneceskoutse@gmail.com>
2024-11-19 16:56:53 -0500

    finish

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,20 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  # read all the tasks
+  get "/tasks", to: "tasks#index", as: :tasks
+
+  # create
+  get "/tasks/new", to: "tasks#new", as: :new
+  post "/tasks", to: "tasks#create", as: :create
+
+  # read one
+  get "/tasks/:id", to: "tasks#show", as: :task
+
+  # update
+  get "/tasks/:id/edit", to: "tasks#edit", as: :edit
+  patch "/tasks/:id", to: "tasks#update"
+
+  # delete
+  delete "/tasks/:id", to: "tasks#destroy"
 end
